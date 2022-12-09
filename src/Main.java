6adeb43cb1021f4bb9d2ed8240e9b3c91083edb8
Nikita Bolshakov <bnikmik@gmail.com>
2022-12-09 12:52:42 +0300

    Самостоятельная работа 1.6: Задача недели 1.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,36 @@
 public class Main {
+    private static void editScript(String[] roles, String[] textLines) {
+        StringBuilder script = new StringBuilder();
+        for (int i = 0; i < roles.length; i++) {
+            script.append(roles[i] + ":\n");
+            for (int j = 0; j < textLines.length; j++) {
+                if (textLines[j].startsWith(roles[i])) {
+                    script.append((j + 1) + ") ");
+                    script.append(textLines[j].substring(roles[i].length() + 2));
+                    script.append("\n");
+                }
+            }
+            script.append("\n");
+        }
+        System.out.println(script);
+    }
+
     public static void main(String[] args) {
+        String[] roles = {
+                "Городничий",
+                "Аммос Федорович",
+                "Артемий Филиппович",
+                "Лука Лукич",
+        };
+        String[] textLines = {
+                "Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
+                "Аммос Федорович: Как ревизор?",
+                "Артемий Филиппович: Как ревизор?",
+                "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.",
+                "Аммос Федорович: Вот те на!",
+                "Артемий Филиппович: Вот не было заботы, так подай!",
+                "Лука Лукич: Господи боже! еще и с секретным предписаньем!",
+        };
+        editScript(roles, textLines);
     }
 }
